c2bf9681402446e50131f905d258b0ca87f0822b
Nishant Pawar <nishantpawar1378@gmail.com>
2024-01-13 02:10:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import TopBar from "./topbar/TopBar";
+import Home from "./pages/home/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      Blog app
-    </div>
+    <>
+    <TopBar/>
+    <Home />
+    
+    </>
   );
 }
 
