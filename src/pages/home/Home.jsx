58da81daa8ce0d57674b7f2014649e7f826c28ce
Nishant Pawar <nishantpawar1378@gmail.com>
2024-01-17 22:16:27 +0530

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,18 @@
 import Header from "../../header/Header";
+import Posts from "../../posts/Posts";
+import Sidebar from "../../sidebar/Sidebar";
 
 import "./home.css"
 
 const Home = () => {
   return (
-    <div className="home">
+    <>
       <Header/>
-      homepage
+    <div className="home">
+    <Posts/>
+      <Sidebar/>
     </div>
+    </>
   )
 }
 
