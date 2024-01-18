823f43e3d5033a6a047093c929d4ee6bb7920ed9
Nishant Pawar <nishantpawar1378@gmail.com>
2024-01-18 19:54:44 +0530

    changes

--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-const Post = ({ img }) => (
-  <img src={img} alt="Post" style={{ maxWidth: '100%', height: 'auto' }} />
-);
+const Posts = () => {
+  return (
+    <div className="posts">
+      posts
+    </div>
+  )
+}
 
-const App = () => (
-  <div>
-    <Post img="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-    <Post img="https://images.pexels.com/photos/6758029/pexels-photo-6758029.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
-    <Post img="https://images.pexels.com/photos/6711867/pexels-photo-6711867.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-    <Post img="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-    <Post img="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-  </div>
-);
-
-export default App;
+export default Posts
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import "./sidebar.css"; 
 
-const sidebar = () => {
+const Sidebar = () => {
   return (
-    <div>
-      
+    <div className="sidebar">
+      <div className="sidebarItem">
+         <span className="sidebarTitle">ABOUT ME</span>
+         <img src="" alt="" />
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias accusamus eum quo quod ipsam placeat explicabo numquam temporibus, repudiandae architecto odio. Laboriosam assumenda hic expedita animi perferendis, earum recusandae?</p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarLisst"> 
+          <li className="sidebarListItem2">Life</li>
+          <li className="sidebarListItem2">Music</li>
+          <li className="sidebarListItem2">Style</li>
+          <li className="sidebarListItem2">Sport</li>
+          <li className="sidebarListItem2">Tech</li>
+          <li className="sidebarListItem2">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+      <span className="sidebarTitle">FOLLOW US</span>
+      <div className="sidebarSocial">
+       <i className=" sidebarIcon fab fa-facebook-square"></i>
+       <i className=" sidebarIcon fab fa-twitter-square"></i>
+       <i className=" sidebarIcon fab fa-pinterest-square"></i>
+       <i className=" sidebarIcon fab fa-instagram-square"></i>
+      </div>
+      </div>
     </div>
   )
 }
 
-export default sidebar
+export default Sidebar
